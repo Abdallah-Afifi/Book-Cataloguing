36558a3c18807bcbcde5c9b5fddb8544302e1812
HadjAhmed <hadj_ahmed@aucegypt.edu>
2024-08-29 12:10:14 +0100

    We now show the detection while scanning the barcode and the frame is taken once a threshold confidence is reached. Finally, I set different values for the progress bar in different stages.

--- PyQT GUI/app.py
+++ PyQT GUI/app.py
@@ -150,80 +150,129 @@ class FirstWindow(QWidget):
 
     def take_front_image(self):
         self.capture_image('front.jpg', 'Capture Front Cover')
+        self.progress_bar.setValue(20)
 
     def take_back_image(self):
         self.capture_image('back.jpg', 'Capture Back Cover')
+        self.progress_bar.setValue(40)
 
     def capture_image(self, filename, window_title):
         cap = cv2.VideoCapture(0)
         if not cap.isOpened():
             print("Error: Could not open camera.")
             return
-
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                print("Failed to grab frame")
-                break
-
-            cv2.imshow(window_title, frame)
-            key = cv2.waitKey(1)
-            if key != -1:
-                cv2.imwrite(filename, frame)
-                break
-
-        cap.release()
-        cv2.destroyAllWindows()
-
-        pixmap = QPixmap(filename)
         if filename == 'front.jpg':
+            while True:
+                ret, frame = cap.read()
+                if not ret:
+                    print("Failed to grab frame")
+                    break
+
+                cv2.imshow(window_title, frame)
+                key = cv2.waitKey(1)
+                if key != -1:
+                    cv2.imwrite(filename, frame)
+                    break
+
+            cap.release()
+            cv2.destroyAllWindows()
+
+            pixmap = QPixmap(filename)
             self.front_image.setPixmap(pixmap.scaled(400, 400, Qt.KeepAspectRatio))
             self.front_taken = True
         else:
+            self.model = YOLO('best.torchscript', task='detect')
+            running = True 
+            while running:
+                ret, frame = cap.read()
+                if not ret:
+                    print("Failed to grab frame")
+                    break
+                # Run the YOLO model on the frame
+                results = self.model(frame)
+                # Extract the predicted bounding boxes and labels
+                for result in results:
+                    for box in result.boxes:
+                        x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())  # Convert tensor to list and then to int
+                        conf = box.conf.item()  # Get the confidence score
+                        label = box.cls.item()  # Get the label
+                        # Draw the bounding box and label on the frame
+                        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                        cv2.putText(frame, f'{label} {conf:.2f}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+                        # Crop the detected barcode region from the frame
+                        cropped_barcode = frame[y1:y2, x1:x2]
+                        # Save the cropped image
+                        if(conf >= 0.82): 
+                            cropped_filename =  'cropped_barcode.png'
+                            cv2.imwrite(cropped_filename, cropped_barcode)
+                            #frame_count += 1q
+                            running = False
+
+
+                # Display the resulting frame
+                # cv2.imshow('YOLOv8 Real-Time Detection', frame)
+                cv2.imshow(window_title, frame)
+                cv2.imwrite(filename, frame)
+                
+                # Break the loop on 'q' key press
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+
+            cap.release()
+            cv2.destroyAllWindows()
+
+            pixmap = QPixmap(filename)
+            self.frame = cv2.imread('back.jpg')
+            self.results = self.model(self.frame)
             self.back_image.setPixmap(pixmap.scaled(400, 400, Qt.KeepAspectRatio))
             self.back_taken = True
         
         self.process_button.setEnabled(self.front_taken and self.back_taken)
 
     def process_images(self):
-        self.progress_bar.setValue(20)
-        self.process_detection()
-        self.progress_bar.setValue(60)
+        # self.progress_bar.setValue(20)
+        # self.process_detection()
         self.read_barcode()
-        self.progress_bar.setValue(100)
+        self.progress_bar.setValue(60)
 
-    def process_detection(self):
-        self.model = YOLO('best.torchscript', task='detect')
-        self.frame = cv2.imread('back.jpg')
-        self.results = self.model(self.frame)
+    # def process_detection(self):
+    #     self.model = YOLO('best.torchscript', task='detect')
+    #     self.frame = cv2.imread('back.jpg')
+    #     self.results = self.model(self.frame)
 
     def read_barcode(self):
-        cropped_barcode = None
-        for result in self.results:
-            for box in result.boxes:
-                x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())
-                conf = box.conf.item()
-                if conf < 0.8: 
-                    continue
-                cropped_barcode = self.frame[y1:y2, x1:x2]
-                cropped_filename = 'cropped_barcode.png'
-                cv2.imwrite(cropped_filename, cropped_barcode)
-                break
-
+        # cropped_barcode = None
+        # for result in self.results:
+        #     for box in result.boxes:
+        #         x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())
+        #         conf = box.conf.item()
+        #         if conf < 0.8: 
+        #             continue
+        #         cropped_barcode = self.frame[y1:y2, x1:x2]
+                
+        #         break
+        cropped_filename = 'cropped_barcode.png'
+        # cv2.imwrite(cropped_filename, cropped_barcode)
+        cropped_barcode = cv2.imread(cropped_filename)
         if cropped_barcode is not None and cropped_barcode.size > 0:
             self.extract_book_info(cropped_barcode)
+            # self.progress_bar.setValue(80)
         else:
             result_text = handle_ocr_and_ai()
+            self.progress_bar.setValue(100)
             self.show_results(result_text)
 
     def extract_book_info(self, cropped_barcode):
+        print("Extracting information from the barcode")
         results = zxingcpp.read_barcodes(cropped_barcode)
         isbn = None
         for result in results:
             isbn = result.text
+            print("isbn = {}", isbn)
             
         if not results:
             result_text = handle_ocr_and_ai()
+            self.progress_bar.setValue(100)
             self.show_results(result_text)
             return
         
@@ -233,10 +282,13 @@ class FirstWindow(QWidget):
         if found:
             result_text = '\n'.join([f"{key}: {value}" for key, value in book_info.items()])
             self.show_results(result_text)
+            
         else:
             result_text = handle_ocr_and_ai()
+            self.progress_bar.setValue(100)
             self.show_results(result_text)
 
+        
     def show_results(self, text):
         self.results_window = ResultsWindow(text)
         self.results_window.show()
